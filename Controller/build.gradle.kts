00c1a4e49ed4e504cc548d45017fa627b16b2eb2
VtlTmkv <vtltmkv@gmail.com>
2021-10-13 21:14:48 +0300

    Вроде работает, добавил обработку аннотаций от ламбокса

--- Controller/build.gradle.kts
+++ Controller/build.gradle.kts
@@ -11,12 +11,12 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains:annotations:20.1.0")
-    compileOnly ("org.jetbrains:annotations:16.0.2")
     implementation("org.projectlombok:lombok:1.18.20")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     implementation (project(":models"))
     annotationProcessor("org.projectlombok:lombok:1.18.20")
+    implementation ("com.google.code.gson:gson:2.8.5")
 }
 
 tasks.getByName<Test>("test") {
--- Controller/src/main/java/library/Application.java
+++ Controller/src/main/java/library/Application.java
@@ -1,12 +1,15 @@
 package library;
 
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
         System.out.println("Введите полное имя автора для поиска. Для выхода введите 1");
         String name = name = sc.nextLine();;
         boolean flag = false;
@@ -14,7 +17,7 @@ public class Application {
         while (!name.equals("1")) {
             for (int i = 0; i < lib.getLibrary().length; i++) {
                 if (lib.getLibrary()[i].getAuthor().getName().equals(name)) {
-                    System.out.println(lib.getLibrary()[i].toString());
+                    System.out.println(gson.toJson(lib.getLibrary()[i]));
                     flag = true;
                 }
             }
@@ -23,6 +26,7 @@ public class Application {
             System.out.println("Введите полное имя автора для поиска. Для выхода введите 1");
             name = sc.nextLine();
         }
+        sc.close();
         System.out.println("Спасибо, что пользовались нашим сервисом");
     }
 }
